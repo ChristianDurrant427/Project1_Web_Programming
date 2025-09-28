1d529599b1923fdc76b6e87eeace97ac31344ec7
ChristianDurrant427 <trombonemaverick02@gmail.com>
2025-09-27 19:22:18 -0600

    adding javascript file to index.html

--- modal.js
+++ modal.js
@@ -1,12 +1,13 @@
 var modal = document.getElementById("ImageModal");
 
-
 var img = document.getElementById("img1")
 
 var modalIMG = document.getElementById("img01")
+
 img.onclick = function() {
     modal.style.display = "block";
     modalIMG.src = this.src;
+    captionText.innerHTML = this.alt;
 }
 
 var span = document.getElementById("close")[0];
